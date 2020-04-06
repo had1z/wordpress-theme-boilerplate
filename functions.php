8d26c44eb8530c8a07c5f32cd573f2875a4556b9
Hadi Zahedian <zahedian@smartmindco.com>
2020-04-06 23:32:58 +0430

    Removed some extra actions from head section

--- functions.php
+++ functions.php
@@ -4,6 +4,20 @@ add_action( 'init', 'hztheme_register_menus' );
 add_action( 'after_setup_theme', 'hztheme_theme_support' );
 add_action( 'wp_enqueue_scripts', 'hztheme_register_styles' );
 add_action( 'wp_enqueue_scripts', 'hztheme_register_scripts' );
+add_action( 'wp_enqueue_scripts', 'hztheme_remove_extra_scripts' );
+
+remove_action( 'wp_head', 'rsd_link' );
+remove_action( 'wp_head', 'wp_generator' );
+remove_action( 'wp_head', 'wlwmanifest_link' );
+remove_action( 'wp_head', 'wp_resource_hints', 2 );
+remove_action( 'wp_head', 'wp_shortlink_wp_head' );
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
+remove_action( 'wp_head', 'wp_oembed_add_discovery_links', 10 );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+remove_action( 'template_redirect', 'rest_output_link_header', 11 );
+
+add_filter( 'show_admin_bar', '__return_false' );
 
 function hztheme_theme_support() {
     add_theme_support( 'post-thumbnails' );
@@ -23,6 +37,11 @@ function hztheme_register_scripts() {
 
     wp_enqueue_script( 'hztheme-uikit-script', get_template_directory_uri() . '/node_modules/uikit/dist/js/uikit.js', array(), $theme_version, true );
     wp_enqueue_script( 'hztheme-custom-script', get_template_directory_uri() . '/assets/js/scripts.js', array(), $theme_version, true );
+}
+
+function hztheme_remove_extra_scripts() {
+    wp_dequeue_style( 'wp-block-library' );
+    wp_dequeue_style( 'wp-block-library-theme' );
 }
 
 function hztheme_register_menus() {
