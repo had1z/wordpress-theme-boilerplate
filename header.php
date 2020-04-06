4996ab5901a4d555b3b581a2409036326d00c288
Hadi Zahedian <zahedian@smartmindco.com>
2020-04-06 15:31:13 +0430

    Added basic header and footer content

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fa_IR">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta name="description" content="<?php bloginfo( 'description' ); ?>">
+    <?php wp_head(); ?>
+    <link rel="icon" href="<?php echo get_stylesheet_directory_uri() . '/favicon.ico'; ?>">
+</head>
+<body>
